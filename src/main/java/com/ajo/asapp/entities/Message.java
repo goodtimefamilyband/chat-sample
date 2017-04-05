9acf082bf27b04bb4dbe97c49b2a5374c423ec56
Andyman <ajo0131@gmail.com>
2017-04-05 03:50:58 -0400

    Add date posted property to message

--- src/main/java/com/ajo/asapp/entities/Message.java
+++ src/main/java/com/ajo/asapp/entities/Message.java
@@ -1,9 +1,12 @@
 package com.ajo.asapp.entities;
 
+import java.sql.Date;
+
 public class Message extends AbstractIdItem<Long>{
 
   private String text;
   private int userId;
+  private Date posted;
   
   public String getText() {
     return text;
@@ -19,6 +22,14 @@ public class Message extends AbstractIdItem<Long>{
   
   public void setUserId(int userId) {
     this.userId = userId;
+  }
+
+  public Date getPosted() {
+    return posted;
+  }
+
+  public void setPosted(Date posted) {
+    this.posted = posted;
   }
   
 }
