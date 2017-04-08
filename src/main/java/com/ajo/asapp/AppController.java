1b0fc38de24129e544586e8d3d8fdc40f2e20e06
Andyman <ajo0131@gmail.com>
2017-04-08 03:43:57 -0400

    Add WebSocket functions to AppController

--- src/main/java/com/ajo/asapp/AppController.java
+++ src/main/java/com/ajo/asapp/AppController.java
@@ -4,17 +4,20 @@ import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.ajo.asapp.entities.Message;
 import com.ajo.asapp.entities.User;
@@ -28,6 +31,13 @@ public class AppController {
 
   @Autowired
   private MessageDao messageDao;
+  
+  private SimpMessagingTemplate template;
+  
+  @Autowired
+  public AppController(SimpMessagingTemplate template) {
+    this.template = template;
+  }
   
   @GetMapping("/")
   public String home(HttpServletRequest req, ModelMap model) {
@@ -45,11 +55,17 @@ public class AppController {
   }
   
   @PostMapping("/app/messages")
-  public void postMessage(HttpServletRequest req, @AuthenticationPrincipal User u, @RequestParam("msg") String msg) {
+  @ResponseStatus(HttpStatus.OK)
+  public void postMessage(HttpServletRequest req, 
+      @AuthenticationPrincipal User u, 
+      @RequestParam("msg") String msg) {
+    System.out.println("Got message "+ msg);
     Message m = new Message();
     m.setText(msg);
     m.setUserId(u.getId());
     messageDao.add(m);
+    
+    this.template.convertAndSend("/app/messaging", m);
   }
   
 }
